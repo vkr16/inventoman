f5300796def2cefe1c7cfeb16b2575d8a4f3686f
Fikri Miftah A <fikri.droid16@gmail.com>
2023-03-10 20:09:40 +0700

    delete .env file from gitignore list

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/inventoman/';
+    public $baseURL = '';
 
     /**
      * --------------------------------------------------------------------------
